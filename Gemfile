663202f8f9e1f816b25816904bd71da2cdee1480
Mostafa Ahangarha <ahangarha@riseup.net>
2024-04-10 01:08:35 +0330

    Show dates in Jalali

--- Gemfile
+++ Gemfile
@@ -11,3 +11,5 @@ gem "jekyll-postcss", "~> 0.5.0"
 gem "csv", "~> 3.3"
 gem "base64", "~> 0.2.0"
 gem "bigdecimal", "~> 3.1"
+
+gem "parsi-date", "~> 0.4.0"
--- _plugins/persian_utils.rb
+++ _plugins/persian_utils.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
+require 'parsi-date'
 
 module Jekyll
   # Set of utility methods for Persian language
   module PersianUtils
     def to_persian_digit(input)
       input.tr('0123456789', '۰۱۲۳۴۵۶۷۸۹')
+    end
+
+    def to_jalali(input, format = nil)
+      jalali_date = input.to_parsi
+
+      return jalali_date.to_s unless format
+
+      jalali_date.strftime(format)
     end
   end
 end
