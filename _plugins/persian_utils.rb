569ff724664620a83c5a0389ad6277f49caae92e
Mostafa Ahangarha <ahangarha@riseup.net>
2024-04-15 10:56:15 +0330

    Add docstring for the plugin methods

--- _plugins/persian_utils.rb
+++ _plugins/persian_utils.rb
@@ -4,14 +4,27 @@ require 'parsi-date'
 module Jekyll
   # Set of utility methods for Persian language
   module PersianUtils
+    #
+    # Convert Latin digits into Persian digits
+    #
+    # @param input [String] the given string to be manipulated
+    #
+    # @return [String] The string with converted digits
+    #
     def to_persian_digit(input)
       input.tr('0123456789', '۰۱۲۳۴۵۶۷۸۹')
     end
 
-    def to_jalali(input, format = nil)
+    #
+    # Convert Gregorian into Jalali date
+    #
+    # @param input [String] Gregorian date
+    # @param format [String] standard strftime format
+    #
+    # @return [String] Jalali date
+    #
+    def to_jalali(input, format = '')
       jalali_date = input.to_parsi
-
-      return jalali_date.to_s unless format
 
       jalali_date.strftime(format)
     end
