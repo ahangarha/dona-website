5a6a9a939541389d30b2892372c4984443baa960
Mostafa Ahangarha <ahangarha@riseup.net>
2024-09-02 22:32:31 +0330

    Improve icons

--- _plugins/media_utils.rb
+++ _plugins/media_utils.rb
@@ -33,24 +33,28 @@ module Jekyll
       def initialize(tag_name, args, tokens)
         super
 
-        @icon_name, @svg_attributes = args.downcase.split(' ', 2)
+        @name, @svg_attributes = args.downcase.split(' ', 2)
 
         @icons = {}
       end
 
       def render(context)
-        @icon_name = context['icon_name']
+        # this gets the value from 'icon_name' variable in the context in which the tag runs
+        @context_icon_name = context['icon_name']
 
-        icon(@icon_name)
+        icon
       end
 
       private
 
-      def icon(name)
-        load_icon(name.split('_').first) || load_icon('default')
+      def icon
+        load_icon(@name) || load_icon(@context_icon_name) || load_icon('default')
       end
 
-      def load_icon(name)
+      def load_icon(fullname)
+        # take the first part of the name. Useful when having generic names
+        name = fullname.split('_').first
+
         return @icons[name] unless @icons[name].nil?
 
         file_path = File.join(File.dirname(__FILE__), "media_utils_icons/#{name}.svg")
